455b8a8a3dfba7c936260e43c50ec421e6f4f76f
Bruno Al. Brito <bruno.brito@startmeup.com.br>
2019-08-12 23:36:29 -0300

    Preparendo spring boot application

---
+++ src/main/java/br/com/kakfachallenger/KakfaChallengerApplication.java
@@ -0,0 +1,11 @@
+package br.com.kakfachallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfaChallengerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KakfaChallengerApplication.class, args);
+	}
+}
